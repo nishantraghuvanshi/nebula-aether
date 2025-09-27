f20c463c175b1113b6221540594ccf4d39efcaf2
Moksh Patil <mokshpatil42@gmail.com>
2025-09-27 07:57:42 +0530

    bugs

--- apps/orchestrator/main.go
+++ apps/orchestrator/main.go
@@ -1779,7 +1779,7 @@ func main() {
 	}
 
 	// Connect to NATS
-	nc, err := nats.Connect("0.tcp.in.ngrok.io:15646")
+	nc, err := nats.Connect("0.tcp.in.ngrok.io:13500")
 	if err != nil {
 		log.Fatalf("Error connecting to NATS: %v", err)
 	}
